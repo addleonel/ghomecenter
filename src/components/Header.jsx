f38a393ec9abbe4383ef60717e149a2b62f28fd1
addleonel <adleonel3633@gmail.com>
2022-06-16 20:35:41 -0500

    no lorem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,7 +93,8 @@ const Header = () => {
       <nav id="navbarroot-id" className="navbarroot">
         <Container fluid="xxl" className="nav-bar"> 
             <Link className="nav-bar__logo" to="/">
-                <img className="nav-bar__logo-image" src={ logo } alt=""/> <span className="nav-bar__logo-text">Granitos Home Center</span>
+              {/* <img className="nav-bar__logo-image" src={ logo } alt=""/> */}
+                 <span className="nav-bar__logo-text">Logo</span>
             </Link>
             <button className="nav-bar__button-item nav-bar__buttons-item--subscribe nav-bar__buttons-item--subscribe-responsive" onClick={ () => window.location.href="/"}>Contactar</button>
             
@@ -102,10 +103,8 @@ const Header = () => {
             </div>
             
             <div className="nav-bar__buttons">
-                <Link className="nav-bar__buttons-item nav-bar__buttons-item--home" to="/">Introducción</Link>
-                <Link className="nav-bar__buttons-item nav-bar__buttons-item--home"  to="/intro">Consumidores</Link>
                 <button id="nav-bar__buttons-item--company-id" className="nav-bar__buttons-item nav-bar__buttons-item--company">Empresa <FontAwesomeIcon icon="fa-solid fa-caret-down" /></button>
-                <Link className="nav-bar__buttons-item nav-bar__buttons-item--business"  to="/intro">Negocios</Link>
+                <Link className="nav-bar__buttons-item nav-bar__buttons-item--business"  to="/intro">Productos</Link>
                 <button className="nav-bar__button-item nav-bar__buttons-item--subscribe" onClick={ () => window.location.href="/"}>Contactar</button>                                                                                                         
             </div>
         </Container>         
@@ -130,11 +129,9 @@ const Header = () => {
         </div>
       <section id="nav-bar__responsive" className="nav-bar__responsive--none"  onClick={ disableMenu }>
         <ul id="nav-bar__responsive-list-id" className="nav-bar__responsive-list" onClick={ enableMenu }>
-            <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/intro#start">Introducción</Link></li>
-            <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/intro#customers">Consumidores</Link></li>
             <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/about">Sobre Nosotros</Link></li>
             <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/contact">Contacto</Link></li>
-            <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/intro#business">Negocios</Link></li>
+            <li className="nav-bar__responsive-li" ><Link className="nav-bar__responsive-item" to="/products">Productos</Link></li>
         </ul>                                                       
       </section>
     </React.Fragment>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,10 +9,6 @@ const Main = () => {
       </section> 
       <div style={{"padding-bottom": "3000px", "background": "#fff"}}>
 
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores eius facilis a reiciendis optio! Quos ipsam labore dolore dicta corrupti quam adipisci cupiditate? Facilis perferendis doloribus cupiditate libero, nam ducimus.
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio corrupti deleniti quis omnis! Accusantium enim, atque ipsum dicta perspiciatis unde molestiae inventore pariatur blanditiis! Dolor perspiciatis sit eum voluptate architecto.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. A quae rem quas reprehenderit natus, facere aliquam iste odio at voluptas dolores quo ex consequuntur suscipit nobis laborum adipisci blanditiis laboriosam.
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo adipisci debitis, cumque ea veritatis possimus ipsum alias, expedita quaerat, nobis obcaecati nihil minus sint inventore ducimus corrupti aperiam omnis dolore!
       </div>
     </React.Fragment>
   );
