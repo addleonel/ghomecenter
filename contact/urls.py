afefc5d10572692b24e2a8a6047e5b140df8a586
addleonel <adleonel3633@gmail.com>
2022-06-16 22:13:22 -0500

    send data

--- contact/urls.py
+++ contact/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name='contact'
+
 urlpatterns = [
     path('', views.welcome_view, name='welcome'),
-    path('contact/', views.contact_view, name='contact'),
+    path('contact/', views.contact_view, name='contact_view'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import ContactModel 
 
 def welcome_view(request):
     return render(request, 'welcome.html')
 
 def contact_view(request):
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST['name'])
+        print(request.POST['surname'])
+        print(request.POST['email'])
+        print(request.POST['message'])
+        
+
+    return render(request, 'contact/contact.html', {})
