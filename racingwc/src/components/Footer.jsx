e7917cedd43e92045e4da102f48cf741d8d65192
addleonel <adleonel3633@gmail.com>
2022-11-14 19:34:33 -0500

    refactoring

--- racingwc/src/components/Footer.jsx
+++ racingwc/src/components/Footer.jsx
@@ -17,11 +17,11 @@ const Footer = () => {
         </ul>
         <ul className="home-footer__ul">
             <p className="home-footer__p"> Nuestras categorías:</p>
-            {/* <li className="home-footer__li"><HashLink  to="/products/#llantas" className="home-footer__link">Llantas</HashLink></li> */}
+            <li className="home-footer__li"><HashLink  to="/products/#llantas" className="home-footer__link">Llantas</HashLink></li>
             <li className="home-footer__li"><HashLink  to="/products/#sistema-arrastre" className="home-footer__link">Sistema de arrastre</HashLink></li>
             <li className="home-footer__li"><HashLink  to="/products/#sistema-electrico" className="home-footer__link">Sistema electrico</HashLink></li>
-            {/* <li className="home-footer__li"><HashLink  to="/products/#sistema-motor" className="home-footer__link">sistema de motor</HashLink></li> */}
-            {/* <li className="home-footer__li"><HashLink  to="/products/#luces" className="home-footer__link">Luces</HashLink></li> */}
+            <li className="home-footer__li"><HashLink  to="/products/#sistema-motor" className="home-footer__link">sistema de motor</HashLink></li>
+            <li className="home-footer__li"><HashLink  to="/products/#luces" className="home-footer__link">Luces</HashLink></li>
             <li className="home-footer__li"><HashLink  to="/products/#accesorios-motor" className="home-footer__link">Accesorios para motor</HashLink></li>
             <li className="home-footer__li"><HashLink  to="/products/#accesorios-piloto" className="home-footer__link">Accesorios para piloto</HashLink></li>
             <li className="home-footer__li"><HashLink  to="/products/#sistemas-frenos" className="home-footer__link">Sistema de frenos</HashLink></li>
@@ -46,7 +46,7 @@ const Footer = () => {
           <p className="home-footer__p"><FontAwesomeIcon icon="fa-solid fa-location-dot" /> jr .tarapaca 918 Huánuco, Peru</p>
         </div>
     </div>
-    <img className="redwoman" src="https://raw.githubusercontent.com/addleonel/ghcenter/main/racingwc/src/assets/static/logo/moto.png" />
+    <img className="redwoman" src="https://raw.githubusercontent.com/addleonel/rx2m/main/racingwc/src/assets/static/logo/moto.png" alt=""/>
     <p className="home-footer__copyright">
       2022 © Rosales Racing Motors - Reservados todos los derechos
     </p>
--- racingwc/src/components/Main.jsx
+++ racingwc/src/components/Main.jsx
@@ -2,10 +2,8 @@ import React , { useState, useEffect } from "react";
 import Slider from "react-slick";
 import { Container } from "react-bootstrap";
 import "../assets/styles/Main.scss";
-// import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import buttonURL from "../utils.js";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // products
 import ProductItem from "./ProductItem";
@@ -234,10 +232,10 @@ const Main = () => {
                             </button>
 						</div>
 						<div className="portal-media">
-							<img className="portal-image pimage-1" src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/271830144_3039380062980739_2031031125507542809_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=tE9hmojjzjkAX-xvlEl&tn=3RXRreKQDGxF6YYQ&_nc_ht=scontent-lim1-1.xx&oh=00_AT96MpbLcXMMsjJ-8LLTq7Kj5D_yIfZh-0etspTbI6i74A&oe=62B657E2" alt="" />
-							<img className="portal-image pimage-2"src="https://scontent-lim1-1.xx.fbcdn.net/v/t39.30808-6/272263958_3042277879357624_3408080719838212641_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=x5P6I9m8fQwAX_TAq0e&tn=3RXRreKQDGxF6YYQ&_nc_ht=scontent-lim1-1.xx&oh=00_AT9KjUXMfQWKGAqLUM0HPxH7rzcMuGU3Sw7_og5xLdLx1A&oe=62B64B83" alt="" />
+							<img className="portal-image pimage-1" src="https://images.pexels.com/photos/5806190/pexels-photo-5806190.jpeg" alt="" />
+							<img className="portal-image pimage-2"src="https://images.pexels.com/photos/2948297/pexels-photo-2948297.jpeg" alt="" />
 							
-							<video id="mp4_src" className="portal-video" src="https://accezby-media-develop.nyc3.digitaloceanspaces.com/video/video1.mp4" autoPlay muted loop></video>
+							{/* <video id="mp4_src" className="portal-video" src="" autoPlay muted loop></video> */}
 						</div>
 					</div>
 				</Container>
@@ -246,12 +244,12 @@ const Main = () => {
 				<Container fluid="xxl">
 					<div className="c-container">
 						<Slider {...sliderState}>
-							{/* <div>
+							<div>
 								<HashLink to="/products/#llantas" className="circle-image">
 									<img style={{borderRadius: "50%"}} src="https://images.pexels.com/photos/5806190/pexels-photo-5806190.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"  alt="" />
 									<p>Llantas</p>
 								</HashLink>
-							</div> */}
+							</div>
 							<div>
 								<HashLink to="/products/#sistema-arrastre" className="circle-image">
 									<img style={{borderRadius: "50%"}} src="https://images.pexels.com/photos/1119796/pexels-photo-1119796.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
@@ -264,18 +262,18 @@ const Main = () => {
 									<p>Sistema electrico</p>
 								</HashLink>
 							</div>
-							{/* <div>
+							<div>
 								<HashLink to="/products/#sistema-motor" className="circle-image">
 									<img style={{borderRadius: "50%"}} src="https://images.pexels.com/photos/8550641/pexels-photo-8550641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
 									<p>Sistema de motor</p>
 								</HashLink>
-							</div> */}
-							{/* <div>
+							</div>
+							<div>
 								<HashLink to="/products/#luces" className="circle-image">
 									<img style={{borderRadius: "50%"}} src="https://images.pexels.com/photos/1253640/pexels-photo-1253640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
 									<p>Luces</p>
 								</HashLink>
-							</div> */}
+							</div>
 							<div>
 								<HashLink to="/products/#accesorios-motor" className="circle-image">
 									<img style={{borderRadius: "50%"}} src="https://images.pexels.com/photos/1407130/pexels-photo-1407130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
@@ -297,13 +295,13 @@ const Main = () => {
 							
 						</Slider>
 					</div>
-					{/* <h2 className="category">Llantas</h2>
+					<h2 className="category">Llantas</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
 							{listLlantas}
 						</Slider>
 						<HashLink style={{textDecoration: "none"}} to="/products/#llantas">Ver más</HashLink>
-					</div> */}
+					</div>
 					<h2 className="category">Sistema de arrastre</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
@@ -318,20 +316,20 @@ const Main = () => {
 						</Slider>
 						<HashLink style={{textDecoration: "none"}}to="/products/#sistema-electrico">Ver más</HashLink>
 					</div>
-					{/* <h2 className="category">Sistema de Motor</h2>
+					<h2 className="category">Sistema de Motor</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
 							{listSistemaMotor}
 						</Slider>
 						<HashLink style={{textDecoration: "none"}}to="/products/#sistema-motor">Ver más</HashLink>
-					</div> */}
-					{/* <h2 className="category">Luces</h2>
+					</div>
+					<h2 className="category">Luces</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
 							{listLuces}
 						</Slider>
 						<HashLink style={{textDecoration: "none"}}to="/products/#luces">Ver más</HashLink>
-					</div> */}
+					</div>
 					<h2 className="category">Accesorios para Motor</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
@@ -346,13 +344,13 @@ const Main = () => {
 						</Slider>
 						<HashLink style={{textDecoration: "none"}}to="/products/#accesorios-piloto">Ver más</HashLink>
 					</div>
-					{/* <h2 className="category">Sistemas de frenos</h2>
+					<h2 className="category">Sistemas de frenos</h2>
 					<div className="c-container">
 						<Slider {...sliderState}>
 							{listSistemaFrenos}
 						</Slider>
 						<HashLink style={{textDecoration: "none"}}to="/products/#sistemas-frenos">Ver más</HashLink>
-					</div> */}
+					</div>
 				</Container>
 			</section>
 		</React.Fragment>
